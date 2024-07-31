64ca13c22c403e2180bc8281c5956ad336f87f2b
mohammad blur <blor1379@gmail.com>
2024-07-31 20:39:06 +0330

    :sparkles: add StreakView

---
+++ SwiftCallenderWidget/Preview Content/StreakView.swift
@@ -0,0 +1,29 @@
+//
+//  StreakView.swift
+//  SwiftCallenderWidget
+//
+//  Created by Mohammad Blur on 7/31/24.
+//
+
+import SwiftUI
+
+struct StreakView: View {
+    
+    @State private var streakValue = 0
+    var body: some View {
+        VStack {
+            Text("\(streakValue)")
+                .font(.system(size: 200, weight: .semibold, design: .rounded))
+                .foregroundStyle(streakValue > 0 ? .orange : .pink)
+            Text("Current Streak")
+                .font(.title2)
+                .bold()
+                .foregroundColor(.secondary)
+        }
+        .offset(y: -50)
+    }
+}
+
+#Preview {
+    StreakView()
+}
