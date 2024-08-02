58218d499c0320758eef59ebf2e897c7bb4dedd6
mohammad blur <blor1379@gmail.com>
2024-08-02 11:57:33 +0330

    :sparkles: add widget

---
+++ SwiftCalWidget/SwiftCalWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftCalWidgetBundle.swift
+//  SwiftCalWidget
+//
+//  Created by Mohammad Blur on 8/2/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SwiftCalWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SwiftCalWidget()
+    }
+}
