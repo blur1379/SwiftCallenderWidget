5d355c0fd339e5a6729522be82bc43720e9b7656
mohammad blur <blor1379@gmail.com>
2024-08-06 23:49:42 +0330

    Create CalendarHeaderView.swift

---
+++ SwiftCallenderWidget/CalendarHeaderView.swift
@@ -0,0 +1,19 @@
+//
+//  CalendarHeaderView.swift
+//  SwiftCallenderWidget
+//
+//  Created by Mohammad Blur on 8/6/24.
+//
+
+import SwiftUI
+
+struct CalendarHeaderView: View {
+    let daysOfWeek = ["S","M","T","W","T","F","S"]
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarHeaderView()
+}
